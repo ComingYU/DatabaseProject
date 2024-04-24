c497e7b4f2f801abc6e344171904614735711809
ComingYU <2608360412@qq.com>
2024-04-24 21:21:27 +0800

    Initial commit

---
+++ tests/db/x.cc
@@ -0,0 +1,15 @@
+#include "../catch.hpp"
+TEST_CASE("db/block.h test")
+{
+    SECTION("size")
+    {
+        REQUIRE(true);
+        // Record rec;
+        // std::vector<struct iovec> iov;
+        // unsigned char header = RECORD_FULL_ALL;
+        // unsigned char buffer[1024];
+        // iov.push_back({buffer, 1024});
+        // rec.set(iov, &header);
+        // rec.get(iov, &header);
+    }
+}
