de51dc23a5c55a212743e9ca29d3a38a25d4e111
Kang Mingyu <132156296+ComingYU@users.noreply.github.com>
2024-04-25 10:10:51 +0800

    Update x.cc

--- tests/db/x.cc
+++ tests/db/x.cc
@@ -4,12 +4,6 @@ TEST_CASE("db/block.h test")
     SECTION("size")
     {
         REQUIRE(true);
-        // Record rec;
-        // std::vector<struct iovec> iov;
-        // unsigned char header = RECORD_FULL_ALL;
-        // unsigned char buffer[1024];
-        // iov.push_back({buffer, 1024});
-        // rec.set(iov, &header);
-        // rec.get(iov, &header);
+    
     }
 }
